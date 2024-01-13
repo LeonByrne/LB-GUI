9985e4726a96c075f1d8e588eea460fafc86f286
Leon Byrne <byrnel58@tcd.ie>
2024-01-13 14:37:34 +0000

    Test of rendering.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include "LB/LB.hpp"
 
+#include <memory>
+
 int main()
 {
   init();
 
   LBwindow test;
+
+  Label *widget = new Label("test");
+
+  widget->setPos(vec2(300.0f));
+  widget->setSize(vec2(50.0f));
+  test.addWidget(widget);
+
+
+  test.setVisible(true);
+
+  // test.setVisible(false);
 
   while(true)
   {
