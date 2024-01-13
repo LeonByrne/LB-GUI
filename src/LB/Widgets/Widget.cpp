157c2897bfc23d639098bbfc6e1d756ce69798fa
Leon Byrne <byrnel58@tcd.ie>
2024-01-13 22:44:16 +0000

    Now a concrete class

--- src/LB/Widgets/Widget.cpp
+++ src/LB/Widgets/Widget.cpp
@@ -12,49 +12,13 @@ Widget::~Widget()
 
 }
 
-/**
- * @brief Try to set it to the specified size, fails if outside min/max sizes
- * 
- * @param size 
- * @return vec2 The size it is actually set to
- */
-vec2 Widget::setSize(const vec2 &size)
-{
-	if(size.x > maxSize.x)
-	{
-		this->size.x = maxSize.x;
-	} else if(size.x < minSize.x)
-	{
-		this->size.x = minSize.x;
-	} else
-	{
-		this->size.x = size.x;
-	}
-
-	if(size.y > maxSize.y)
-	{
-		this->size.y = maxSize.y;
-	} else if(size.y < minSize.y)
-	{
-		this->size.y = minSize.y;
-	} else
-	{
-		this->size.y = size.y;
-	}
-
-	return this->size;
-}
-
-void Widget::setPos(const vec2 &pos)
-{
-	this->pos = pos;
-}
-
 void Widget::draw()
 {
-	glViewport(pos.x, pos.y, size.x, size.y);
-	glScissor(pos.x, pos.y, size.x, size.y);
+	glViewport(x, y, width, height);
+	glScissor(x, y, width, height);
 
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(colour.w, colour.x, colour.y, colour.z);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
+
+
--- src/LB/Widgets/Widget.hpp
+++ src/LB/Widgets/Widget.hpp
@@ -8,24 +8,22 @@ class Widget
 {
 private:
 	/* data */
-	
-	// The centre of it.
-	vec2 pos;
-	vec2 size;
 
+protected:
 	vec2 minSize;
 	vec2 maxSize;
 
 public:
-	Widget(/* args */);
+	// These are manipulated by their containers freely, best left public for this
+	int x, y;
+	int width, height;
 
+	// Background colour
+	vec4 colour;
+
+public:
+	Widget(/* args */);
 	~Widget();
 
-	// makes a copy of the widget.
-	virtual Widget* copy() = 0;
-
 	virtual void draw();
-
-	vec2 setSize(const vec2 &size);
-	void setPos(const vec2 &pos);
 };
